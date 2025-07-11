a7be1bfa26907a4b217435420bebd245c775bc01
송유현 <68471917+songyouhyun@users.noreply.github.com>
2025-07-11 20:34:07 +0900

    폴더 구조 변경

--- prisma/migrations/20250711112752_init/migration.sql
+++ prisma/migrations/20250711112752_init/migration.sql
@@ -3,9 +3,8 @@ CREATE TABLE "Cafe" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "address" TEXT NOT NULL,
-    "opening_hours" TEXT NOT NULL,
-    "bean_updated_at" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Cafe_pkey" PRIMARY KEY ("id")
 );
@@ -15,7 +14,6 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "nickname" TEXT NOT NULL,
-    "role" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -23,26 +21,26 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "Lineup_report" (
+CREATE TABLE "Lineup" (
     "id" SERIAL NOT NULL,
-    "status" TEXT NOT NULL,
-    "image_url" TEXT,
-    "reported_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "image_url" TEXT NOT NULL,
     "reported_date" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
     "cafe_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
 
-    CONSTRAINT "Lineup_report_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Lineup_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Lineup_report_user_id_cafe_id_reported_date_key" ON "Lineup_report"("user_id", "cafe_id", "reported_date");
+CREATE UNIQUE INDEX "Lineup_user_id_cafe_id_reported_date_key" ON "Lineup"("user_id", "cafe_id", "reported_date");
 
 -- AddForeignKey
-ALTER TABLE "Lineup_report" ADD CONSTRAINT "Lineup_report_cafe_id_fkey" FOREIGN KEY ("cafe_id") REFERENCES "Cafe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Lineup" ADD CONSTRAINT "Lineup_cafe_id_fkey" FOREIGN KEY ("cafe_id") REFERENCES "Cafe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Lineup_report" ADD CONSTRAINT "Lineup_report_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Lineup" ADD CONSTRAINT "Lineup_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CafesModule } from './cafes/cafes.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [CafesModule],
+  imports: [PrismaModule, CafesModule],
   controllers: [AppController],
   providers: [AppService],
 })
