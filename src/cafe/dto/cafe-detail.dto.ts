37674c02654d3e17f682ddd2234ac4fe42b243eb
송유현 <68471917+songyouhyun@users.noreply.github.com>
2025-08-10 01:14:06 +0900

    카페 상세 조회 필드명 변경

--- src/cafe/dto/cafe-detail.dto.ts
+++ src/cafe/dto/cafe-detail.dto.ts
@@ -1,18 +1,28 @@
 import { Lineup } from "generated/prisma";
 import { CafeWithLineup } from "../types/cafe-with-lineup";
 
+type LineupItem = {
+  id: number;
+  imageFilename: string;
+  reportedDate: Date;
+}
+
 export class CafeDetailDto {
   id: number;
   name: string;
   address: string;
-  lineup: Lineup[];
+  lineup: LineupItem[];
 
   static from(cafe: CafeWithLineup): CafeDetailDto {
     const dto = new CafeDetailDto();
     dto.id = cafe.id;
     dto.name = cafe.name;
     dto.address = cafe.address;
-    dto.lineup = cafe.lineup;
+    dto.lineup = cafe.lineup.map((item: Lineup) => ({
+      id: item.id,
+      imageFilename: item.image_filename,
+      reportedDate: item.reported_date,
+    }));
     return dto;
   }
 }
