b5b66cf39852627948558035d7abe4fa155e3253
송유현 <68471917+songyouhyun@users.noreply.github.com>
2025-08-17 05:02:03 +0900

    Prisma 쿼리 로깅

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma';
 
 @Injectable()
@@ -6,6 +6,17 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: [
+        { level: 'query', emit: 'event' },
+        { level: 'info', emit: 'stdout' },
+        { level: 'warn', emit: 'stdout' },
+        { level: 'error', emit: 'stdout' },]
+    });
+    this.$on('query', (e) => console.log(`[prisma] ${e.query} ${e.params} +${e.duration}ms`));
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
