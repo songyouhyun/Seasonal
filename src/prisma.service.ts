82daa50fbcd5038c8a39ac1be672521b1ed250fb
송유현 <68471917+songyouhyun@users.noreply.github.com>
2025-08-17 13:30:49 +0900

    merge

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -14,11 +14,7 @@ export class PrismaService
         { level: 'warn', emit: 'stdout' },
         { level: 'error', emit: 'stdout' },]
     });
-<<<<<<< HEAD
     this.$on('query', (e) => console.log(`[prisma] ${e.query} ${e.params} +${e.duration}ms`));
-=======
-    this.$on('query', (e) => console.log(`[prisma] ${e.query} +${e.duration}ms`));
->>>>>>> b0f00b7 (Prisma 쿼리 로깅)
   }
 
   async onModuleInit() {
